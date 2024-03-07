5d7064f963bc66f64e14081d89038561caace99e
Merabtine Aya Malek <ma_merabtine@esi.dz>
2024-03-07 22:20:59 +0100

    Client/Se connecter

--- backend/routes/client.js
+++ backend/routes/client.js
@@ -3,5 +3,6 @@ const clientController = require('../controllers/client.controller');
 
 const router = express.Router();
 router.post('/sign-up',clientController.signUp);
+router.post('/login',clientController.login);
 
 module.exports = router;
