3a6081f4899d9cd32c077fcbb14ee1db8245673e
Merabtine Aya Malek <ma_merabtine@esi.dz>
2024-03-09 12:20:06 +0100

    inscription

--- backend/controllers/client.controller.js
+++ backend/controllers/client.controller.js
@@ -13,29 +13,43 @@ function signUp(req, res) {
                 message: "Compte email existant"
             });
         } else {
-            bcryptjs.genSalt(10, function (err, salt) {
-                bcryptjs.hash(req.body.MotdepasseClient, salt, function (err, hash) {
-                    const client = {
-                        NomClient: req.body.NomClient,
-                        PrenomClient: req.body.PrenomClient,
-                        MotdepasseClient: hash,
-                        EmailClient: req.body.EmailClient,
-                        AdresseClient: req.body.AdresseClient,
-                        NumeroTelClient: req.body.NumeroTelClient
-                    }
-                    models.Client.create(client).then(result => {
-                        res.status(201).json({
-                            message: "Inscription client réussite",
-                            client: result
-                        });
-                    }).catch(error => {
-                        res.status(500).json({
-                            message: "Something went wrong",
-                            error: error
+            models.Artisan.findOne({
+                where: {EmailArtisan:req.body.EmailClient }
+            }).then(result=>{
+                if (result) {
+                    res.status(409).json({
+                        message: "Compte email existant"
+                    });
+                }else{   bcryptjs.genSalt(10, function (err, salt) {
+                    bcryptjs.hash(req.body.MotdepasseClient, salt, function (err, hash) {
+                        const client = {
+                            NomClient: req.body.NomClient,
+                            PrenomClient: req.body.PrenomClient,
+                            MotdepasseClient: hash,
+                            EmailClient: req.body.EmailClient,
+                            AdresseClient: req.body.AdresseClient,
+                            NumeroTelClient: req.body.NumeroTelClient
+                        }
+                        models.Client.create(client).then(result => {
+                            res.status(201).json({
+                                message: "Inscription client réussite",
+                                client: result
+                            });
+                        }).catch(error => {
+                            res.status(500).json({
+                                message: "Something went wrong",
+                                error: error
+                            });
                         });
                     });
+                });}
+            }).catch(error => {
+                res.status(500).json({
+                    message: "Something went wrong",
+                    error: error
                 });
             });
+         
         }
     }).catch(error => {
         res.status(500).json({
