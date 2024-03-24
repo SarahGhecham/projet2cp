f4c970d993e0b042d46d6328d8d347c718b2ddd7
Djouaher Yasmine <my_djouaher@esi.dz>
2024-03-24 19:26:30 +0100

    Page Connexion

--- frontend/flutter_application_proj2cp/lib/main.dart
+++ frontend/flutter_application_proj2cp/lib/main.dart
@@ -1,125 +1,194 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(const MyApp());
+  // WidgetsApp //MaterialApp //CupertinoApp
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.green,
+    ),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-  // This widget is the root of your application.
+class _HomePageState extends State<HomePage> {
+
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.white,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            SizedBox(height: 100),
+            Container(
+              height:100,
+              width:300,
+              child: Image.asset("assets/logo.png"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 30),
+            Center(
+              child: Text(
+                "Connexion",
+                style: TextStyle(
+                  color: Color(0xFF05564B),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  fontFamily: "Poppins",
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            Container(
+              width: 277,
+              height: 41,
+              child: TextFormField(
+                controller: _usernameController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  hintStyle: TextStyle(fontFamily: "Poppins", color: Color(0xFF777777),),
+                  filled: true,
+                  fillColor: Color(0xFFDCC8C5).withOpacity(0.22),
+                  contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0,),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFDCC8C5), width: 1.0,),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            Container(
+              width: 277,
+              height: 41,
+              child: TextFormField(
+                controller: _passwordController,
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  hintStyle: TextStyle(fontFamily: "Poppins", color: Color(0xFF777777),),
+                  filled: true,
+                  fillColor: Color(0xFFDCC8C5).withOpacity(0.22),
+                  contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0,),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFFDCC8C5), width: 1.0,),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: (){},
+              child: Text("Connexion",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: "Poppins"
+                ),
+              ),
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all<Size>(Size(100, 37)),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFFF8787)),
+              ),
+            ),
+            SizedBox(height: 30),
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: Divider(
+                      color: Color(0xFFDDDDDD),
+                      thickness: 1.0,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Text("or",style: TextStyle(color: Colors.grey, fontSize: 16, fontFamily: "Poppins"),),
+                  SizedBox(width: 10),
+                  SizedBox(
+                    width: 150,
+                    child: Divider(
+                      color: Color(0xFFDDDDDD),
+                      thickness: 1.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset("assets/apple.svg", width: 30, height: 30,),
+                    SizedBox(height: 5), // Adjust the height as needed
+                  ],
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Connexion avec Apple",
+                      style: TextStyle(fontSize: 16, fontFamily: "Poppins",),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset("assets/google.svg", width: 25, height: 25,),
+                    SizedBox(height: 5), // Adjust the height as needed
+                  ],
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Connexion avec Google",
+                      style: TextStyle(fontSize: 16, fontFamily: "Poppins",),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: 70),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Vous n’avez pas de compte client ?",style: TextStyle(color: Color(0xFF777777), fontFamily: "Poppins", fontSize: 16,),),
+                Text("S'inscrire",style: TextStyle(color: Color(0xFF05564B), fontFamily: "Poppins", fontWeight: FontWeight.bold, fontSize: 16,),),
+
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
