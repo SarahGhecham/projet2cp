97c83ae7216a41cf215ab4b515759c264a92d226
assiakheddia <assia.kheddia@icloud.com>
2024-03-26 21:28:45 +0000

    bottom navigation bar done (on doit modifier l icon de home apr)

---
+++ frontend/flutter_application_proj2cp/lib/constants/constants.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+const vertFonce = Color(0xff05564b);
+const vertClair = Color(0xffd6e3dc);
+const creme = Color(0xffdcc8c5);
+const crevette = Color(0xffff8787);
+const kWhite = Color(0xfffFFFFF);
+const kBlack = Color(0xff000000);
