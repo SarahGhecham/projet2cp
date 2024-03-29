975cfe7b7fe22d90ebf0b7c287294a0deec2f1ac
Merabtine Aya Malek <ma_merabtine@esi.dz>
2024-03-29 16:49:52 +0100

    Détails Demande Confirmée

--- backend/routes/client.js
+++ backend/routes/client.js
@@ -17,6 +17,6 @@ router.get('/test/:id',clientController.test);
 router.get('/AfficherActiviteTerminee',auth(),clientController.Activiteterminee);
 router.get('/AfficherActiviteEncours',auth(),clientController.ActiviteEncours);
 router.get('/AfficherPrestations',auth(),clientController.AfficherPrestations);
-router.get('/DetailsRDVConfirme',auth(),clientController.DetailsRDVConfirme);
+router.get('/DetailsDemandeConfirmee',auth(),clientController.DetailsDemandeConfirmee);
 
 module.exports = router;
