ddebb3513e36ac0c4b7380d01d49efbbf2e0d1c7
lynda boudraa <you@example.com>
2024-03-31 00:39:56 +0100

    conflits

--- frontend/flutter_application_proj2cp/lib/app.dart
+++ frontend/flutter_application_proj2cp/lib/app.dart
@@ -1,35 +1,17 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_application_proj2cp/constants/constants.dart';
-import 'package:flutter_application_proj2cp/pages/pagesentree.dart';
-import 'package:flutter_application_proj2cp/widgets/bottom_nav_bar.dart';
+// ignore_for_file: unused_import
 
-import 'package:flutter_application_proj2cp/pages/home_page.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:PROJET2CP/pages/profile_screen.dart';
+import 'package:PROJET2CP/afficher_peinture.dart';
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
-    return ScreenUtilInit(
-      designSize: const Size(393, 808),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      // Use builder only if you need to use library outside ScreenUtilInit context
-      builder: (context, child) {
-        return const GetMaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Beaver',
-          /*theme: ThemeData(
-            iconTheme: const IconThemeData(color: vertClair),
-          ),*/
-          home: BottomNavBar(),
-        );
-      },
-
-      //child: const HomePage(title: 'First Method'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Profile(),
     );
   }
 }
