36aab1eb212da34c1cbad671ad3bbf5cec9e3aaa
Boudraalynda <163526430+Boudraalynda@users.noreply.github.com>
2024-03-30 01:36:10 +0100

    Update profile_screen.dart

--- frontend/flutter_application_proj2cp/lib/Screens/profile_screen.dart
+++ frontend/flutter_application_proj2cp/lib/Screens/profile_screen.dart
@@ -169,110 +169,111 @@ void _saveChanges() {
                 ),
               ],
             ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height / 2.5 - 260,
-                left: (MediaQuery.of(context).size.width - 380) / 2,
-              ),
-              child: SizedBox(
-                width: 98,
-                height: 33,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_isEditing) {
-                      _saveChanges();
-                      _toggleEditing(false);
-                    } else {
-                      _toggleEditing(true);
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(14),
-                    ),
-                    backgroundColor: const Color(0xFFFF8787),
-                  ),
-                  child: Text(
-                    _isEditing ? 'Valider' : 'Editer',
-                    style: const TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontSize: 14,
-                      fontFamily: 'Lato',
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 72.0), 
+                  child: SizedBox(
+                    width: 98,
+                    height: 33,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_isEditing) {
+                          _saveChanges();
+                          _toggleEditing(false);
+                        } else {
+                          _toggleEditing(true);
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14),
+                        ),
+                        backgroundColor: const Color(0xFFFF8787),
+                      ),
+                      child: Text(
+                        _isEditing ? 'Valider' : 'Editer',
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 14,
+                          fontFamily: 'Lato',
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: 30),
-            Positioned(
-              bottom: 80,
-              left: 20,
-              child: Container(
-                width: 170,
-                height: 61,
-                decoration: BoxDecoration(
-                  color: Color(0xFFD6E3DC),
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(
-                    color: Color(0xFFDCC8C5),
-                    width: 1,
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Services',
-                          style: TextStyle(
-                            color: Color(0xFFFF8787),
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          _userData['services_count'].toString(),
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      width: 1,
-                      height: double.infinity,
+                SizedBox(height: 30),
+                Container(
+                  width: 170,
+                  height: 61,
+                  margin: EdgeInsets.only(left: 00, bottom: 00),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFD6E3DC),
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
                       color: Color(0xFFDCC8C5),
+                      width: 1,
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          ' Points',
-                          style: TextStyle(
-                            color: Color(0xFFFF8787),
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Service',
+                            style: TextStyle(
+                              color: Color(0xFFFF8787),
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        Text(
-                          _userData['points'].toString(),
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
+                          Text(
+                            _userData['services_count'].toString(),
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                      Container(
+                        width: 1,
+                        height: double.infinity,
+                        color: Color(0xFFDCC8C5),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Points',
+                            style: TextStyle(
+                              color: Color(0xFFFF8787),
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            _userData['points'].toString(),
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
+           
             Column(
               children: [
                 SizedBox(height: 30),
