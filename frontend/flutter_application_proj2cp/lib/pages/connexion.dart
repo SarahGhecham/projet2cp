c7eb6c8808bc2ff645157b4697e64d74e3a53e4a
Djouaher Yasmine <my_djouaher@esi.dz>
2024-04-03 21:32:35 +0200

    fixed

--- frontend/flutter_application_proj2cp/lib/pages/connexion.dart
+++ frontend/flutter_application_proj2cp/lib/pages/connexion.dart
@@ -66,225 +66,227 @@ class _LogInPageState extends State<LogInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 100),
-            Container(
-              height: 100,
-              width: 300,
-              child: Image.asset("assets/logo.png"),
-            ),
-            SizedBox(height: 30),
-            Center(
-              child: Text(
-                "Connexion",
-                style: GoogleFonts.poppins(
-                  color: Color(0xFF05564B),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 100),
+              Container(
+                height: 100,
+                width: 300,
+                child: Image.asset("assets/logo.png"),
               ),
-            ),
-            SizedBox(height: 50),
-            Container(
-              width: 277,
-              height: 41,
-              decoration: BoxDecoration(
-                color: Color(0xFFDCC8C5).withOpacity(0.22),
-                border: Border.all(
-                  color: Color(0xFFDCC8C5),
-                  width: 2,
-                ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextFormField(
-                controller: _usernameController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  hintText: "Email",
-                  hintStyle: TextStyle(
-                    fontFamily: "Poppins",
-                    color: Color(0xFF777777),
-                  ),
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 16.0,
-                  ),
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
-            SizedBox(height: 40),
-            Container(
-              width: 277,
-              height: 41,
-              decoration: BoxDecoration(
-                color: Color(0xFFDCC8C5).withOpacity(0.22),
-                border: Border.all(
-                  color: Color(0xFFDCC8C5),
-                  width: 2,
-                ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextFormField(
-                controller: _passwordController,
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: "Mot de passe",
-                  hintStyle: GoogleFonts.poppins(
-                    color: Color(0xFF777777),
-                  ),
-                  contentPadding: EdgeInsets.symmetric(
-                    vertical: 10.0,
-                    horizontal: 16.0,
-                  ),
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: _authenticateUser,
-              child: Text(
-                "Connexion",
-                style: GoogleFonts.poppins(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              style: ButtonStyle(
-                minimumSize: MaterialStateProperty.all<Size>(Size(100, 37)),
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                backgroundColor:
-                    MaterialStateProperty.all<Color>(Color(0xFFFF8787)),
-              ),
-            ),
-            SizedBox(height: 30),
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 150,
-                    child: Divider(
-                      color: Color(0xFFDDDDDD),
-                      thickness: 1.0,
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    "or",
-                    style: GoogleFonts.poppins(
-                      color: Colors.grey,
-                      fontSize: 16,
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  SizedBox(
-                    width: 150,
-                    child: Divider(
-                      color: Color(0xFFDDDDDD),
-                      thickness: 1.0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                      "assets/apple.svg",
-                      width: 30,
-                      height: 30,
-                    ),
-                    SizedBox(height: 5), // Adjust the height as needed
-                  ],
-                ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Connexion avec Apple",
-                      style: GoogleFonts.poppins(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(
-                      "assets/google.svg",
-                      width: 25,
-                      height: 25,
-                    ),
-                    SizedBox(height: 5), // Adjust the height as needed
-                  ],
-                ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Connexion avec Google",
-                      style: GoogleFonts.poppins(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 70),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Vous n’avez pas de compte client ?",
+              SizedBox(height: 30),
+              Center(
+                child: Text(
+                  "Connexion",
                   style: GoogleFonts.poppins(
-                    color: Color(0xFF777777),
+                    color: Color(0xFF05564B),
+                    fontWeight: FontWeight.bold,
                     fontSize: 16,
                   ),
                 ),
-                GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SignUpPage()));
-                    },
-                    child: Text(
-                      "S'inscrire",
+              ),
+              SizedBox(height: 50),
+              Container(
+                width: 277,
+                height: 41,
+                decoration: BoxDecoration(
+                  color: Color(0xFFDCC8C5).withOpacity(0.22),
+                  border: Border.all(
+                    color: Color(0xFFDCC8C5),
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextFormField(
+                  controller: _usernameController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    hintStyle: TextStyle(
+                      fontFamily: "Poppins",
+                      color: Color(0xFF777777),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 16.0,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              Container(
+                width: 277,
+                height: 41,
+                decoration: BoxDecoration(
+                  color: Color(0xFFDCC8C5).withOpacity(0.22),
+                  border: Border.all(
+                    color: Color(0xFFDCC8C5),
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextFormField(
+                  controller: _passwordController,
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Mot de passe",
+                    hintStyle: GoogleFonts.poppins(
+                      color: Color(0xFF777777),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 10.0,
+                      horizontal: 16.0,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: _authenticateUser,
+                child: Text(
+                  "Connexion",
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all<Size>(Size(100, 37)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Color(0xFFFF8787)),
+                ),
+              ),
+              SizedBox(height: 30),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 150,
+                      child: Divider(
+                        color: Color(0xFFDDDDDD),
+                        thickness: 1.0,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "or",
                       style: GoogleFonts.poppins(
-                        color: Color(0xFF05564B),
-                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
                         fontSize: 16,
                       ),
-                    )),
-              ],
-            ),
-          ],
+                    ),
+                    SizedBox(width: 10),
+                    SizedBox(
+                      width: 150,
+                      child: Divider(
+                        color: Color(0xFFDDDDDD),
+                        thickness: 1.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "assets/apple.svg",
+                        width: 30,
+                        height: 30,
+                      ),
+                      SizedBox(height: 5), // Adjust the height as needed
+                    ],
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Connexion avec Apple",
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "assets/google.svg",
+                        width: 25,
+                        height: 25,
+                      ),
+                      SizedBox(height: 5), // Adjust the height as needed
+                    ],
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Connexion avec Google",
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 70),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Vous n’avez pas de compte client ?",
+                    style: GoogleFonts.poppins(
+                      color: Color(0xFF777777),
+                      fontSize: 16,
+                    ),
+                  ),
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignUpPage()));
+                      },
+                      child: Text(
+                        "S'inscrire",
+                        style: GoogleFonts.poppins(
+                          color: Color(0xFF05564B),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      )),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
