929c48c696bddc22193ec7b3adc0bf6620a9e96f
assiakheddia <assia.kheddia@icloud.com>
2024-04-05 15:43:37 +0100

    removed bottom nav bar from app.dart

--- frontend/flutter_application_proj2cp/lib/app.dart
+++ frontend/flutter_application_proj2cp/lib/app.dart
@@ -26,7 +26,7 @@ class App extends StatelessWidget {
           /*theme: ThemeData(
             iconTheme: const IconThemeData(color: vertClair),
           ),*/
-          home: BottomNavBar(),
+          home: LogInPage(),
         );
       },
 
