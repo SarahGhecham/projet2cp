599c299f9f93e9f1358bed96e943f49ecd9653c5
Merabtine Aya Malek <ma_merabtine@esi.dz>
2024-05-01 12:12:52 +0100

    adresse ip

--- frontend/flutter_application_proj2cp/lib/app.dart
+++ frontend/flutter_application_proj2cp/lib/app.dart
@@ -54,7 +54,7 @@ class App extends StatelessWidget {
             iconTheme: const IconThemeData(color: vertClair),
           ),*/
 
-          home: LogInPage(),
+          home: SignUpPage(),
         );
       },
 
