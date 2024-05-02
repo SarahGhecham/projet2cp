586e2695559ca3b0d8ae517c0a12838276296500
assiakheddia <assia.kheddia@icloud.com>
2024-05-02 01:26:54 +0100

    merge assia et main

--- frontend/flutter_application_proj2cp/lib/app.dart
+++ frontend/flutter_application_proj2cp/lib/app.dart
@@ -67,7 +67,7 @@ class App extends StatelessWidget {
               iconTheme: const IconThemeData(color: vertClair),
             ),*/
 
-            home: BottomNavBarAdmin(),
+            home: LogInPage(),
           ),
         );
       },
